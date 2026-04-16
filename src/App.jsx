a7921e593165009d74e7b8b7b82db25bd90c137a
Chabzz <martinchavy383@gmail.com>
2026-04-16 11:44:37 +0800

    setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>I am Chavy Martin B. Tapuyao</h1>
+      <h1>Full Stack Developer  || Database Developer</h1>
+      <h1>Iligan City, Lanao del Norte, Philippines</h1>
+      <h1>martinchavy383@gmail.com</h1>
+      <h1>09154000997</h1>
+    </>
+  )
+}
+
+export default App
